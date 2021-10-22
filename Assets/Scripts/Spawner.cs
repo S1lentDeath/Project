05681c66de34f626c073783209e4b844d44928af
DoNotCounter <danila.nemykin@gmail.com>
2021-10-22 22:21:11 +0600

    Init

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private GameObject _enemy;
+    
+    private Transform []_points;
+
+    private void Start()
+    {
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+
+        StartCoroutine(CreateEnemies());
+    }
+
+    private IEnumerator CreateEnemies()
+    {
+        float runningTime = 2f;
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            yield return new WaitForSeconds(runningTime);
+
+            Instantiate(_enemy, _points[i]);
+        }        
+    }
+}
